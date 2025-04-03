1b1661ffbaaae935bb32997380cb3d86686c73ff
maxicassarino <maximocassarino@gmail.com>
2025-04-03 20:46:58 -0300

    Clase 4

---
+++ Clase 4/script.js
@@ -0,0 +1,17 @@
+async function Pokemons() {
+    const div = document.getElementById('pokemonInfo');
+    div.innerHTML = ""; // Limpia los resultados anteriores
+
+    for (let i = 0; i < 3; i++) {
+        const randomId = Math.floor(Math.random() * 898) + 1;
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+        const data = await response.json();
+
+        const pokemonCard = document.createElement('div');
+        pokemonCard.innerHTML = `
+            <h2>${data.name.toUpperCase()}</h2>
+            <img src="${data.sprites.front_default}" alt="${data.name}">
+        `;
+        div.appendChild(pokemonCard);
+    }
+}
